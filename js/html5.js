3f78cfbf582d3a1bf39bc5ff4f8d60b7cb116827
ElJoker63 <eljoker630@gmail.com>
2021-09-22 03:11:36 -0400

    index.html
    ...

---
+++ js/html5.js
@@ -0,0 +1,10 @@
+// JavaScript Document
+document.createElement('header');
+document.createElement('nav');
+document.createElement('section');
+document.createElement('article');
+document.createElement('figure');
+document.createElement('figcaption');
+document.createElement('hgroup');
+document.createElement('aside');
+document.createElement('footer');
